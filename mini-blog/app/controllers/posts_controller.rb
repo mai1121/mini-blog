ec5bb6fd1d7aebe777110e5e3d2a36725f9e56a2
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-04 20:15:14 +0900

    Set actions for posts controller

--- mini-blog/app/controllers/posts_controller.rb
+++ mini-blog/app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+ private
+
+
+
+
+
 end
